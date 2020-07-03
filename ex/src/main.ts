b4d480d90fd6f4e816d0cd540d03d3475c8e9101
Jakub Freisler <jakubfreisler@phrase.com>
2020-07-03 19:53:55 +0200

    feat: add validation

--- ex/src/main.ts
+++ ex/src/main.ts
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import { ValidationProvider, extend } from 'vee-validate'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
 import { required, email } from 'vee-validate/dist/rules';
 
+extend('required', {
+  ...required,
+  message: 'The field "{_field_}" is required'
+});
 extend('email', email);
-extend('required', required);
-
 
 Vue.config.productionTip = false
 
 Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 new Vue({
   router,
--- ex/src/views/Home.vue
+++ ex/src/views/Home.vue
@@ -5,24 +5,24 @@
     <Modal :visible="modalVisible" @close="modalVisible = false">
       <template #header>Typescript Rocks!</template>
       <template #content>
-        <form @submit.prevent="checkForm" novalidate>
-          <p v-if="errors.length">
-            <b>Please correct the following error(s):</b>
-            <ul>
-              <li v-for="error in errors" :key="error">{{ error }}</li>
-            </ul>
-          </p>
-          <ValidationProvider rules="required" v-slot="{ errors }">
-            <input v-model="email" type="email" name="qwe" required placeholder="What's your email address?">
-            <span>{{ errors[0] }}</span>
-          </ValidationProvider>
-          <select v-model="reason" required>
-            <option v-for="(item, key) in reasons" :key="key" :value="key">
-              {{item}}
-            </option>
-          </select>
-          <button>Submit</button>
-        </form>
+        <ValidationObserver v-slot="{ handleSubmit, invalid }">
+          <form
+            @submit.prevent="handleSubmit(onSubmit)"
+            novalidate
+          >
+            <ValidationProvider v-slot="{ errors }">
+              <input v-model="email" required name="email" type="email" placeholder="what's your email address?">
+              <span>{{ errors[0] }}</span>
+            </ValidationProvider>
+            <ValidationProvider v-slot="{ errors }">
+              <select name="reason" v-model="reason" required>
+                <option v-for="(value, key) in reasons" :key="key">{{value}}</option>
+              </select>
+              <span>{{ errors[0] }}</span>
+            </ValidationProvider>
+            <button :disabled="invalid">Submit</button>
+          </form>
+        </ValidationObserver>
       </template>
     </Modal>
   </div>
@@ -43,36 +43,21 @@ export default {
     onTypescriptClick() {
       this.modalVisible = true;
     },
-    checkForm () {
-      this.errors = [];
-
-      if (!this.email) {
-        this.errors.push('Email is required!');
-      } else if (this.email.indexOf('@') === -1) {
-        this.errors.push('Incorrect email format!')
-      }
-
-      if (!this.reason) {
-        this.errors.push('Reason is required!');
-      }
-
-      if (!this.errors.length) {
-        // submit the form
-      }
+    onSubmit () {
+      console.log('submit!');
     }
   },
   data() {
     return {
-      errors: [],
       msg: 'Welcome to Your Vue.js App',
       modalVisible: false,
       email: '',
       reason: '',
       reasons: {
         '': '',
-        'rocks': 'because it rocks',
-        'script': 'because it ends with "script"',
-        'types': 'TYPEEEES'
+        'types': 'TYPEEEES',
+        'script': 'it has "script" in its name',
+        't': 'it start with a T'
       }
     };
   }
